6dbe6b5ec2a943b59602c27dd98d6b520f82170e
sleepingsaint <suryasantosh14523@gmail.com>
2020-10-20 01:45:14 +0530

    Updates

---
+++ precache-manifest.5dede33b2a80d3c4a520edf8356594ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4284fff15500cf2a500551b710b9ac",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f661b20f5e9e69ae0f0f",
+    "url": "/covid-tracker/static/js/2.41430913.chunk.js"
+  },
+  {
+    "revision": "6bdf6becedb14e4609964580f4c3faf6",
+    "url": "/covid-tracker/static/js/2.41430913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb3dd2e150ebb7cee24",
+    "url": "/covid-tracker/static/js/main.d1869577.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid.d7265326.png"
+  }
+]);
